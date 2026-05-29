3be01c96cf639b77d5eca5d8cd790cf74dd85f72
Roger <rogerleecormier@gmail.com>
2026-05-29 18:16:52 -0400

    feat: configure authClient baseURL using window location origin

--- src/auth/client.ts
+++ src/auth/client.ts
@@ -2,6 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
   plugins: [adminClient()],
 });
 
